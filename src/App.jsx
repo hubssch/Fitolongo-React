2ca7340370f5a2e5941068963367bc9e3a2637cd
Hubert S <hubert.schmidt@microsoft.wsei.edu.pl>
2024-11-08 20:05:37 +0100

    Header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,11 @@
-import { useState } from 'react'
+import { } from 'react'
 import './App.css'
+import Header from './Components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <app> <div className="green-belt">
-      <h1 id="app-name">Fitolongo</h1>
-    </div>
-      <div className="container">
-        <div className="box" id="kalendarz">
-          <h1 className="header">Kalendarz łasucha</h1>
-          <p>
-            Licz kalorie i kontroluj nawodnienie, <br />
-            aby masa nie spadła
-          </p>
-          <img
-            src="./images/undraw_breakfast_psiw.svg"
-            alt="Food"
-            className="image"
-          />
-        </div>
-        <div className="box" id="cwiczenia">
-          <h1 className="header">
-            Co zrobić, żeby <br />chude szczury były w strachu
-          </h1>
-          <p>
-            Sprawdź jakie ćwiczenia wykonywać,<br />
-            aby na dyskotece wygrać każdą cepeliadę.
-          </p>
-          <img
-            src="./images/undraw_healthy_habit_kwe6.svg"
-            alt="Big boy training"
-            className="image"
-          />
-        </div>
-        <div className="box" id="trenerzy">
-          <h1 className="header">
-            Oni zrobią z ciebie <br />
-            Wielkiego Chłopa
-          </h1>
-          <p>
-            To tutaj zobaczysz kto może ci pomóc <br />
-            w drodze po marzenia, aby stać na czele łańcucha pokarmowego!
-          </p>
-          <img
-            src="./images/undraw_personal_trainer_re_cnua.svg"
-            alt="Personal trainer"
-            className="image"
-          />
-        </div>
-      </div>
-      <footer className="green-belt" id="footer">
-        Stań się Wielki, spraw by chude szczury były w strachu
-      </footer></app>
+    <Header />
   )
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import { } from 'react'
+import '../App.css'
+
+export default function Header() {
+    return (
+        <div className="w-full h-36 bg-green-800 flex items-center justify-center">
+            <h1 className="text-white text-4xl font-bold" id="app-name">
+                Fitolongo
+            </h1>
+        </div>
+    );
+}
