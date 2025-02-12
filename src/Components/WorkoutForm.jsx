5dc6a770fb5f7c797dd9f235c520555bcdafde33
Miłosz Smoleń <101010230+MiloszSmolen@users.noreply.github.com>
2025-02-12 03:26:41 +0100

    Update WorkoutForm
    Trainer and unlogged user cant use "dziennik aktywności".
    
    Also the profile picture of user was removed from profile settings.

--- src/Components/WorkoutForm.jsx
+++ src/Components/WorkoutForm.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { supabase } from '../supabaseClient';
 
@@ -8,8 +7,35 @@ export default function WorkoutForm({ selectedDate, onWorkoutAdded, editingWorko
     const [reps, setReps] = useState('');
     const [weight, setWeight] = useState('');
     const [personalRecord, setPersonalRecord] = useState(false);
+    const [user, setUser] = useState(null);
+    const [isUser, setIsUser] = useState(false);
 
     useEffect(() => {
+        const fetchUser = async () => {
+            const userId = localStorage.getItem('user_id');
+            if (userId) {
+                const { data: userData, error } = await supabase
+                    .from('users')
+                    .select(`id`)
+                    .eq('id', userId)
+                    .single();
+
+                if (userData && !error) {
+                    setUser(userData);
+
+                    // Sprawdzenie, czy jest trenerem
+                    const { data: trainerData } = await supabase
+                        .from('trainers')
+                        .select(`id`)
+                        .eq('id', userId)
+                        .single();
+
+                    setIsUser(!trainerData); // Jeśli nie znajduje się w trenerach, to jest zwykłym użytkownikiem
+                }
+            }
+        };
+        fetchUser();
+
         if (editingWorkout) {
             setExerciseName(editingWorkout.exercise_name);
             setSets(editingWorkout.sets);
@@ -31,8 +57,12 @@ export default function WorkoutForm({ selectedDate, onWorkoutAdded, editingWorko
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const dateUTC = new Date(Date.UTC(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate()));
+        if (!isUser) {
+            alert('Aby używać dziennika ćwiczeń musisz być zalogowany jako użytkownik.');
+            return;
+        }
 
+        const dateUTC = new Date(Date.UTC(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate()));
         const payload = {
             exercise_name: exerciseName,
             sets: parseInt(sets),
@@ -40,22 +70,18 @@ export default function WorkoutForm({ selectedDate, onWorkoutAdded, editingWorko
             weight: parseFloat(weight),
             personal_record: personalRecord,
             date: dateUTC,
-            user_id: localStorage.getItem('user_id')
+            user_id: user.id
         };
 
         if (editingWorkout) {
-            // Aktualizacja istniejącego ćwiczenia
             const { error } = await supabase.from('workouts').update(payload).match({ id: editingWorkout.id });
-
             if (!error) {
                 onWorkoutAdded();
-                setEditingWorkout(null); // Reset stanu edytowanego ćwiczenia i formularza
+                setEditingWorkout(null);
                 resetForm();
             }
         } else {
-            // Dodawanie nowego ćwiczenia
             const { error } = await supabase.from('workouts').insert([payload]);
-
             if (!error) {
                 onWorkoutAdded();
                 resetForm();
@@ -63,31 +89,37 @@ export default function WorkoutForm({ selectedDate, onWorkoutAdded, editingWorko
         }
     };
 
+    if (!isUser) return (
+    <div className="text-center p-4 max-w-md mx-auto mt-10 rounded-lg border border-red-300 bg-red-200 text-red-700">
+        <h2 className="font-bold text-xl mb-2">Ograniczony dostęp:</h2>
+        <p className="text-lg">Aby używać dziennika ćwiczeń musisz być zalogowany jako użytkownik.</p>
+    </div>)
+
     return (
         <form onSubmit={handleSubmit} className="p-4 bg-white dark:bg-gray-700 rounded-lg shadow-md mt-4">
-            <h3 className="text-lg font-semibold mb-4">{editingWorkout ? 'Edytuj szczegóły treningu' : 'Dodaj szczegóły treningu'} dla {selectedDate.toDateString()}</h3>
-            <div className="mb-2">
-                <label className="block">Nazwa ćwiczenia</label>
-                <input type="text" value={exerciseName} onChange={(e) => setExerciseName(e.target.value)} className="w-full p-2 border rounded text-black" required />
-            </div>
-            <div className="mb-2">
-                <label className="block">Serie</label>
-                <input type="number" value={sets} onChange={(e) => setSets(e.target.value)} className="w-full p-2 border rounded text-black" required />
-            </div>
-            <div className="mb-2">
-                <label className="block">Powtórzenia</label>
-                <input type="number" value={reps} onChange={(e) => setReps(e.target.value)} className="w-full p-2 border rounded text-black" required />
-            </div>
-            <div className="mb-2">
-                <label className="block">Waga (kg)</label>
-                <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} className="w-full p-2 border rounded text-black" required />
-            </div>
-            <div className="mb-4">
-                <label className="block">Rekord osobisty</label>
-                <input type="checkbox" checked={personalRecord} onChange={(e) => setPersonalRecord(e.target.checked)} className="mr-2" />
-                Tak
-            </div>
-            <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded">{editingWorkout ? 'Zaktualizuj ćwiczenie' : 'Dodaj ćwiczenie'}</button>
-        </form>
+        <h3 className="text-lg font-semibold mb-4">{editingWorkout ? 'Edytuj szczegóły treningu' : 'Dodaj szczegóły treningu'} dla {selectedDate.toDateString()}</h3>
+        <div className="mb-2">
+            <label className="block">Nazwa ćwiczenia</label>
+            <input type="text" value={exerciseName} onChange={(e) => setExerciseName(e.target.value)} className="w-full p-2 border rounded text-black" required />
+        </div>
+        <div className="mb-2">
+            <label className="block">Serie</label>
+            <input type="number" value={sets} onChange={(e) => setSets(e.target.value)} className="w-full p-2 border rounded text-black" required />
+        </div>
+        <div className="mb-2">
+            <label className="block">Powtórzenia</label>
+            <input type="number" value={reps} onChange={(e) => setReps(e.target.value)} className="w-full p-2 border rounded text-black" required />
+        </div>
+        <div className="mb-2">
+            <label className="block">Waga (kg)</label>
+            <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} className="w-full p-2 border rounded text-black" required />
+        </div>
+        <div className="mb-4">
+            <label className="block">Rekord osobisty</label>
+            <input type="checkbox" checked={personalRecord} onChange={(e) => setPersonalRecord(e.target.checked)} className="mr-2" />
+            Tak
+        </div>
+        <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded">{editingWorkout ? 'Zaktualizuj ćwiczenie' : 'Dodaj ćwiczenie'}</button>
+    </form>
     );
 }
